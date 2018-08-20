57c951464df1e2da7e641aea1aabc80a3281168a
Joey-Ren <18182561979@163.com>
2018-08-20 22:19:26 +0800

    master

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,101 @@
+<template>
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" tepe="text" placeholder="输入城市名或拼音">
+            
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item of list" :key="item.id" @click="handleCityClick(item.name)">{{item.name}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+import {mapMutations} from 'vuex'
+    export default {
+        name:'CitySearch',
+        props: {
+            cities:Object
+        },
+        computed:{
+            hasNoData(){
+                return !this.list.length
+            }
+        },
+        data () {
+            return {
+                keyword:'',
+                list: [],
+                timer:null
+            }
+        },
+        watch: {
+            keyword () {
+                if(this.timer) {
+                    clearTimeout(this.timer)
+                }
+                this.timer=setTimeout(()=>{
+                    if(!this.keyword){
+                        this.list = [];
+                        return
+                    }
+                    const result = []
+                    for (let i in this.cities){
+                        this.cities[i].forEach(value => {
+                            if(value.spell.indexOf(this.keyword)>-1 || value.name.indexOf(this.keyword)>-1){
+                                result.push(value)
+                            }
+                        })
+                    }
+                    this.list=result
+                },100)
+            }
+        },
+        methods: {
+            handleCityClick(city){
+                //this.$store.commit('changeCity',city)
+                this.changeCity(city)
+                this.$router.push('/')
+            },
+            ...mapMutations(['changeCity'])
+        },
+        mounted() {
+            this.scroll = new Bscroll(this.$refs.search)
+        },
+    }
+</script>
+
+<style lang="stylus" scoped>
+ @import '~styles/varibles.styl'
+    .search
+        padding:0 .1rem
+        height:.72rem
+        background:$bgColor
+      .search-input
+        box-sizing: border-box
+        width: 100%
+        height:.62rem
+        padding: 0 .1rem
+        line-height:.62rem
+        text-align: center
+        border-radius: .06rem
+        color:#666
+    .search-content
+        overflow: hidden
+        position: absolute
+        top: 1.58rem
+        left: 0
+        right: 0
+        bottom: 0
+        z-index: 1
+        background: #eee
+        .search-item
+            line-height:.62rem
+            padding-left:.2rem
+            background:#fff
+            color:#666
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <router-link to="/city">
+            <div class="header-right">
+                {{this.doubleCity}}
+                <span class="iconfont arrow-icon">&#xe64a;</span>
+            </div>
+        </router-link>
+    </div>
+</template>
+
+<script>
+import {mapState, mapGetters} from 'vuex'
+export default {
+  name: 'HomeHeader',
+  computed: {
+      ...mapState(['city']),
+      ...mapGetters(['doubleCity'])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        display :flex
+        line-height:$headerHeight
+        background:$bgColor
+        .header-left
+            width :.64rem
+            float:left
+          .back-icon
+              text-align :center
+              font-size:.4rem
+              color:#fff
+        .header-input
+            flex :1
+            margin-top .12rem
+            margin-left:.2rem
+            padding-left:.2rem
+            height :.64rem
+            line-height :.64rem
+            background :#fff
+            border-radius :.1rem
+            color :#ccc
+        .header-right
+            min-width 1.04rem
+            padding: 0 .1rem
+            float:right
+            text-align :center
+            color:#fff
+          .arrow-icon
+              margin-left :-.04rem
+              font-size:.24rem
+              color:#fff
+</style>
