d0ecf89ba86308f6817dd193f1d0acad6f9a5789
“Joey-Ren” <18182561979@163.com>
2018-08-22 14:05:56 +0800

    animation finished

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,18 @@
+<template>
+    <transition>
+        <slot></slot>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: 'FadeAnimation'
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .v-enter, .v-leave-to
+        opacity: 0
+    .v-enter-active, .v-leave-active
+        reansition: opacity .5s
+</style>
